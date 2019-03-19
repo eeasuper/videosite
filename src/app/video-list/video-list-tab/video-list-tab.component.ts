5fdfcd5c23b7984bb122f35f20a5b42da40ba8b0
Dup <eeasuper@gmail.com>
2019-03-19 22:05:52 +0900

    edited video-list to make video listing show properly

--- src/app/video-list/video-list-tab/video-list-tab.component.ts
+++ src/app/video-list/video-list-tab/video-list-tab.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,Input,OnChanges,ElementRef,Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-video-list-tab',
   templateUrl: './video-list-tab.component.html',
   styleUrls: ['./video-list-tab.component.css']
 })
-export class VideoListTabComponent implements OnInit {
+export class VideoListTabComponent implements OnInit,OnChanges {
 
   @Input('value') value:object;
-  @Input('index') index:number;
+  @Input('videoId') videoId:number;
+  @Input('videos') numOfVid:number;
 
-  constructor() { }
+  constructor(private renderer:Renderer2,private element:ElementRef) { }
 
   ngOnInit() {
   }
 
+  ngOnChanges(){
+    if(this.videoId >= this.numOfVid){
+      // this.renderer.setStyle(this.element.nativeElement, 'transform', 
+        // 'translate(' + ((this.videoId -1) * 100)+'%'+',-130%)');
+      // this.renderer.setStyle(this.element.nativeElement, 'visibility', 'hidden');
+      // this.renderer.setStyle(this.element.nativeElement, 'position', 'absolute');
+    }
+  }
 }
--- src/app/video-list/video-list.component.ts
+++ src/app/video-list/video-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit,Input,HostListener,Renderer2 } from '@angular/core';
+import { Component, OnInit,Input,HostListener,Renderer2,ElementRef,ViewChild} from '@angular/core';
+import {SidebarService} from '../services/sidebar.service';
 
 @Component({
   selector: 'app-video-list',
@@ -7,50 +8,62 @@ import { Component, OnInit,Input,HostListener,Renderer2 } from '@angular/core';
 })
 export class VideoListComponent implements OnInit {
 
+  @ViewChild('videosContainer') private videoCon:ElementRef;
   @Input('data') private data:any;
   private objectValues = Object.values;
-  // private videos:object;
-  // private videos2:any = [];
-  // private debounce:number = 0;
+  private debounce:number = 0;
+  private numOfVid:number;
 
-  // @HostListener('window:resize', ['$event'])
-  // limitVideos(){
+  // getMarginLeft(isSidebarOpen:boolean){
   //   let windowWidth = window.innerWidth;
-  //   if(this.debounce === 0){
-  //     this.debounce = window.setTimeout(()=>{
-  //       if(windowWidth <=850){
-  //         if(this.objectValues(this.data.list).length > 4) {
-  //           this.videos2 = this.objectValues(this.data.list).reduce((acc:any[],cur,i)=>{
-  //             console.log(cur);  
-  //             if(i > 3){
-  //               acc.push(cur);
-  //             }
-  //             return acc;
-  //           },[]);
-  //           this.videos = this.objectValues(this.data.list).reduce((acc:any[],cur,i)=>{
-  //             if(i<=3){
-  //               acc.push(cur);
-  //             }
-  //             return acc;
-  //           },[])
-  //           console.log(this.videos2);
-  //           console.log(this.videos);
-  //         }      
-  //       }
-  //       if(windowWidth){
-
-  //       }
-
-  //       this.debounce = 0;
-  //     },300);
+  //   let sb = 0;
+  //   //sb & isSidbarOpen might be unnecessary...(check later)
+  //   if(isSidebarOpen){sb = 200}
+  //   this.numOfVid = Math.floor((windowWidth - sb)/210);
+  //   let remainderWidth = this.numOfVid*210 + sb;
+  //   // let nextRemainderWidth = ((this.numOfVid-1)*210) + sb;
+  //   let mgLeft = (((windowWidth - remainderWidth)/210)*0.075) *100;
+  //   if(this.numOfVid >= this.objectValues(this.data.list).length){
+  //     return 0;
   //   }
+  //   // if(!isSidebarOpen){
+  //   //   return (((windowWidth - remainderWidth)/210)*0.075) *100;
+  //   //   // return 0;
+  //   // }
+  //   return mgLeft;
   // }
 
-  constructor(private renderer:Renderer2) { }
+  @HostListener('window:resize', ['$event'])
+  limitVideos(){
+    let windowWidth = window.innerWidth;
+    if(this.debounce === 0){
+      this.debounce = window.setTimeout(()=>{
+        let sb = 0;
+        if(this.isSidebarOpen){
+          sb = 200;
+        }
+        [1260,1050,840,630,420].forEach((val, ind, arr)=>{
+          if(windowWidth < val + sb && windowWidth > arr[ind+1] + sb){
+            this.renderer.setStyle(this.videoCon.nativeElement, 'width', arr[ind+1] + 'px');
+          }
+        })
+        // let mgLeft = this.getMarginLeft(this.isSidebarOpen);
+        // this.renderer.setStyle(this.videoCon.nativeElement, 'margin-left', mgLeft+'rem');
+        this.debounce = 0;
+      },300);
+    }
+
+  }
+  private isOverflowing;
+  private isSidebarOpen;
+
+  constructor(private renderer:Renderer2,private element:ElementRef,private sidebar:SidebarService) { }
 
   ngOnInit() {
-    // this.videos = this.data;
-    // this.limitVideos()
+    this.sidebar.change.asObservable().subscribe(isOpen=>{
+      this.isSidebarOpen = isOpen;
+    })
+    this.limitVideos()
   }
 
 }
